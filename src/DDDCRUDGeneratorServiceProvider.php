33516b5c595ab4dae4e9eeee581723f3270156c5
rahamatj <32268831+rahamatj@users.noreply.github.com>
2026-03-14 13:19:34 +0600

    first commit

---
+++ src/DDDCRUDGeneratorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Raham\MyPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class DDDCRUDGeneratorServiceProvider extends ServiceProvider
+{
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
+
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    //
+  }
+}
